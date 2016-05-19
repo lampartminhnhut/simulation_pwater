94e2e5426ed3d49bd2b5e7e93b05c7aa5e9f6bc3
minh_son <minh_son@lampart-vn.com>
2016-05-19 10:06:49 +0700

    Add controller Contract and information

---
+++ application/views/contract/index.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to Contract</title>
+</head>
+<body>
+
+<div id="container">
+    <h1 style="text-align:center">This is Administrator Page</h1>
+    <div id="body" style="width: 70%; margin: 0 auto;">
+    <div style="border: solid 1px; padding: 10px">
+<p>In order for a contract to be formed, the parties must reach mutual assent (also called a meeting of the minds).</p>
+
+<p>This is typically reached through offer and an acceptance which does not vary the offer's terms, which is known as the "mirror image rule".</p>
+
+<p>If a purported acceptance does vary the terms of an offer, it is not an acceptance but a counteroffer and, therefore, simultaneously a rejection of the original offer.</p>
+
+<p>The Uniform Commercial Code disposes of the mirror image rule in §2-207, although the UCC only governs transactions in goods in the USA</p>
+    </div>
+
+</div>
+
+</body>
+</html>
---
+++ application/views/information/index.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to Contract</title>
+</head>
+<body>
+
+<div id="container">
+    <h1 style="text-align:center">This is Administrator Page</h1>
+    <div id="body" style="width: 70%; margin: 0 auto;">
+    <div style="border: solid 1px; padding: 10px">
+<p>Information (shortened as info) is that which informs. In other words, it is the answer to a question of some kind.</br>
+It is also that from which data and knowledge can be derived, as data represents values attributed to parameters, and knowledge signifies understanding of real things or abstract concepts.[1]</br>
+As it regards data, the information's existence is not necessarily coupled to an observer (it exists beyond an event horizon, for example), while in the case of knowledge, the information requires a cognitive observer.</p>
+
+<p>At its most fundamental, information is any propagation of cause and effect within a system.</br>
+Information is conveyed either as the content of a message or through direct or indirect observation of some thing.</br>
+That which is perceived can be construed as a message in its own right, and in that sense, information is always conveyed as the content of a message.</p>
+    </div>
+
+</div>
+
+</body>
+</html>
