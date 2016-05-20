a39365ae67d0494a2f8d0ffb93488f021846c365
minh_nhut <minh_nhut@lampart-vn.com>
2016-05-20 10:50:33 +0700

    refs #10505 Change text.

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -15,8 +15,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <p>A user is a person who uses a computer or network service. Users generally use a system or a software product without the technical expertise required to fully understand it.[1] Power users use advanced features of programs, though they are not necessarily capable of computer programming and system administration.[2][3]</p>
 
 <p>A user often has a user account and is identified to the system by a username (or user name). Other terms for username include login name, screen name (or screenname), nickname (or nick) and handle, which is derived from the identical Citizen's Band radio term.</p>
-
-<p>Some software products provide services to other systems and have no direct end users.</p>
+<!-- [Start][10505][CHG] - [minh_nhut] - [2016-05-19] - description -->
+<!-- <p>Some software products provide services to other systems and have no direct end users.</p> -->
+Some usability professionals have expressed their dislike of the term "user", proposing it to be changed.[8] Don Norman stated that "One of the horrible words we use is 'users'. I am on a crusade to get rid of the word 'users'. I would prefer to call them 'people'."[9]
+<!-- [End][10505][CHG] - [minh_nhut] - [2016-05-19] - description -->
     </div>
 
 </div>
